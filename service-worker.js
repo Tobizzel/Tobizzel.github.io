c41f6850d11db1dc0ae660dc0e394b00b0e1987c
Tobias <tobias_schiller@aol.de>
2019-05-14 12:47:21 +0200

    Service-worker v3

--- service-worker.js
+++ service-worker.js
@@ -4,14 +4,15 @@ var cacheVersion = 1;
 var currentCache = {
   offline: 'offline-cache' + cacheVersion
 };
-const offlineUrl = 'offline-page.html';
+const offlineUrl = 'offline.html';
 
 this.addEventListener('install', event => {
   event.waitUntil(
     caches.open(currentCache.offline).then(function(cache) {
       return cache.addAll([
           './offline.html',
-          './style.css'
+          './style.css',
+          offlineUrl
       ]);
     })
   );
