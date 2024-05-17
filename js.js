7a9a10f370d5668bd8202e33af1a8016ee15aeaa
Tobizzel <49964765+Tobizzel@users.noreply.github.com>
2024-05-17 17:37:35 +0200

    Content added
    Funktion fürs auftauchen der Links + Styling

--- js.js
+++ js.js
@@ -1,45 +1,30 @@
-/* console.log("Noice");
-
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
- */
-var player;
-function onYoutubeIframeAPIReady(){
-    player = new YT.Player('player', {
-        height: 360,
-        width: 640,
-        videoId: 'dQw4w9WgXcQ',
-        /* playerVars: {
-            autoplay: 1,
-            controls: 0,
-            showinfo: 0,
-            modestbranding: 0,
-            loop: 1,
-            fs: 0,
-            autohide: 0
-        }, */
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
-    });
+function myfunction(){
+    var element = document.getElementById('Mute');
+    element.style.transition='all 2s ease';
+    element.style.left = '85vw';
 }
 
-function onPlayerReady(event){
-    event.target.playVideo();
-}
-
-var done = false;
-function onPlayerStateChange(event){
-    if (event.data == YT.PlayerState.PLAYING && !done){
-        setTimeout(stopVideo, 6000);
-        done=true;
+document.addEventListener("DOMContentLoaded", function(){
+    var div = document.getElementById('contentwrapper');
+    var timeout;
+    
+    function resetTimer(){
+        clearTimeout(timeout);
+        div.classList.remove("hidden");
+        timeout = setTimeout(hideDiv, 4000);
     }
-}
 
-function stopVideo(){
-    player.stopVideo()
-}
+    function hideDiv(){
+        // div.style.display="none";
+        div.classList.add("hidden");
+    }
+
+    document.addEventListener("mousemove", resetTimer);
+
+    resetTimer()
+})
+
+/* document.addEventListener("mousemove", function(){
+    var div = document.getElementById('contentwrapper');
+    div.style.display="block";
+}) */
